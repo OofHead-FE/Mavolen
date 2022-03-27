ca2e0642ffc474bd83a06b7a238325c6d2e78ba3
OofHead-FE <98631383+OofHead-FE@users.noreply.github.com>
2022-03-26 20:10:19 -0500

    Update whitelistlol.lua

--- whitelistlol.lua
+++ whitelistlol.lua
@@ -1,8 +1,8 @@
 local wls = {
-    1 = 2427782845
+    k1 = 2427782845
 }
 
 local plr = game.Players.LocalPlayer
-if plr.UserId == wls.1 then
+if plr.UserId == wls.k1 then
     _G.mavolenPrivate = true
 end
