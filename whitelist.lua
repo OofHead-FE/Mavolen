004446fab05188626ed487da9bed4a950831cf3e
OofHead-FE <98631383+OofHead-FE@users.noreply.github.com>
2022-03-28 16:38:27 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,13 +1,15 @@
 local wls = {
+    ROBLOX = 1,
     k1 = 2427782845,
     k2 = 3422192405,
     k3 = 3139920287,
     k4 = 3358804692,
-    k5 = 3421560156
+    k5 = 3421560156,
+    k6 = 3399376797
 }
 
 local plr = game.Players.LocalPlayer
-if plr.UserId == wls.k1 or wls.k2 or wls.k3 or wls.k4 or wls.k5 then
+if plr.UserId == wls.ROBLOX wls.k1 or wls.k2 or wls.k3 or wls.k4 or wls.k5 or wls.k6 then
     _G.sigmapr = true
     _G.mavolenPrivate = true
 end
