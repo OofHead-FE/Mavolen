64242b767759ae40f193a512ce9b7c2dd3cb3db8
OofHead-FE <98631383+OofHead-FE@users.noreply.github.com>
2022-03-28 10:41:10 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,10 +1,11 @@
 local wls = {
     k1 = 2427782845,
-    k2 = 3422192405
+    k2 = 3422192405,
+    k3 = 3139920287
 }
 
 local plr = game.Players.LocalPlayer
-if plr.UserId == wls.k1 or wls.k2 then
+if plr.UserId == wls.k1 or wls.k2 or wls.k3 then
     _G.sigmapr = true
     _G.mavolenPrivate = true
 end
